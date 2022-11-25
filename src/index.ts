5d58679a5a4133ee94bc9e52430ce528d6a07b83
Moumirrai <72272440+Moumirrai@users.noreply.github.com>
2022-11-25 21:49:04 +0100

    express server

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,21 @@
 import { Core } from './struct/Core';
+import express from 'express';
 
 const client = new Core();
 
 (async () => {
+  expressServer();
   await client.main();
 })();
+
+
+async function expressServer() {
+  const app = express();
+  const port = 8080;
+  app.get('/', (req, res) => {
+    res.send('Hello World!');
+  });
+  app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+  });
+}
