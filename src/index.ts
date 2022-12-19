f99ba957524e15d4ec134349903c6356118b655f
Moumirrai <72272440+Moumirrai@users.noreply.github.com>
2022-12-19 10:03:16 +0000

    cleanup

--- src/index.ts
+++ src/index.ts
@@ -4,18 +4,5 @@ import express from 'express';
 const client = new Core();
 
 (async () => {
-  expressServer();
   await client.main();
 })();
-
-
-async function expressServer() {
-  const app = express();
-  const port = 8080;
-  app.get('/', (req, res) => {
-    res.send('Hello World!');
-  });
-  app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
-  });
-}
